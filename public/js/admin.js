65a1b5674dc49adb08e08c43086a13e31f5007fb
alain-ubuntu <alain.jouve@wanadoo.fr>
2025-09-12 17:43:20 +0200

    amelioration de l'interface graphique de l'admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,16 +1,16 @@
 //Pour que les noms de fichiers s'affichent dans le champ de choix de fichier lors d'un upload
 var selectElement=null;
 window.onload = function() {
-    selectElement = document.getElementById("OdpfEditionsPassees_affiche_file");
-    selectElement.addEventListener("change", (event) => {
-        let inputFile = event.currentTarget;
-        $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
-        var sizeFile = document.getElementsByClassName('input-group-text')
-        //$(sizeFile[1]).html(inputFile.files[0].size);
-    });
+    if(document.getElementById("OdpfEditionsPassees_affiche_file")!==null) {
+        selectElement = document.getElementById("OdpfEditionsPassees_affiche_file");
+        selectElement.addEventListener("change", (event) => {
+            let inputFile = event.currentTarget;
+            $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+            var sizeFile = document.getElementsByClassName('input-group-text')
+            //$(sizeFile[1]).html(inputFile.files[0].size);
+        });
+    }
 }
 $(document).ready(function () {
-
-
     $('#modalinfo').modal('show');
 });
